d95ebb75c3ce60490eaacf40263b74305d51b42d
1kJae <wnstjq62@naver.com>
2024-07-09 20:42:09 +0900

    forth commit

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const COUNTRY = 'kR';
-const API_KEY = '656de5ef24e441f2919d26d1342916ee'
-let news = []
+// const API_KEY = `656de5ef24e441f2919d26d1342916ee`;
 
+let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`https://noona-times-24-07-09.netlify.app/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
+    // const url = new URL(`https://noona-times-24-07-09.netlify.app/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
+    // let url = `https://noona-times-24-07-09.netlify.app/top-headlines`
+    
+    const url = new URL(
+        `https://third-js-project-sw-copy.netlify.app/top-headlines?country=us`
+    ); 
+
     const response = await fetch(url)
-    const data = await response.json()
-    news = data.articles
-    console.log("dddd", news)
-};   
+    const data = await response.json();
+    news = data.articles;
+    console.log("ddd", news);
+} 
 
 getLatestNews();
