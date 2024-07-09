340c90546f1316f637fe62ef732a16645785a7c4
1kJae <wnstjq62@naver.com>
2024-07-09 19:35:37 +0900

    third commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = '656de5ef24e441f2919d26d1342916ee'
 let news = []
 
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-24-07-09.netlify.app/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles
