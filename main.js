35c767ff2089796b436c5f351d97809d669b1466
1kJae <wnstjq62@naver.com>
2024-07-09 19:33:15 +0900

    second commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+const COUNTRY = 'kR';
 const API_KEY = '656de5ef24e441f2919d26d1342916ee'
 let news = []
+
 const getLatestNews = async () => {
-    const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    );
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles
