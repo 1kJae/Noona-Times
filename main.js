3e19ef09aed27d41661af5f13944227a14753a4c
1kJae <wnstjq62@naver.com>
2024-07-12 00:39:45 +0900

    sixth commmit

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 // const API_KEY = `656de5ef24e441f2919d26d1342916ee`;
 
-let newList = [];
+let newsList = [];
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+
+let searchInput = document.getElementById("search-input");
+searchInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    getNewsByKeyword();
+  }
+});
+
+const getNewsByKeyword =async () => {
+  let keyWord = document.getElementById("search-input").value
+  url = new URL(`https://third-js-project-sw-copy.netlify.app/top-headlines?country=us&q=${keyWord}`)
+  const response = await fetch(url)
+  const data = await response.json()
+  newsList = data.articles
+  render();
+}
+
 const getLatestNews = async () => {
     // const url = new URL(`https://noona-times-24-07-09.netlify.app/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`);
     // let url = `https://noona-times-24-07-09.netlify.app/top-headlines`
@@ -11,10 +30,21 @@ const getLatestNews = async () => {
 
     const response = await fetch(url)
     const data = await response.json();
-    newList = data.articles;
+    newsList = data.articles;
     render()
-    console.log("ddd", newList);
+    console.log("ddd", newsList);
 } 
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category", category)
+  const url = new URL(`https://third-js-project-sw-copy.netlify.app/top-headlines?country=us&category=${category}`)
+  const response = await fetch(url)
+  const data = await response.json();
+  newsList = data.articles;
+  console.log("ddd", data)
+  render();
+}
 
 const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
@@ -34,7 +64,7 @@ const openSearchBox = () => {
 };
 const otherImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
 const render = () => {
-    const newsHTML = newList.map(
+    const newsHTML = newsList.map(
         (news) => `<div class="row news">
         <div class="col-lg-4">
             <img class="news-img-size" src="${news.urlToImage}"||"${otherImage}"/>
